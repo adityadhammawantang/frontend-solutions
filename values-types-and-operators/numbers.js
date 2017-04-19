2f0e8e713e8d56fea6c8555058e7de7aeac131da
adityadhammawantang <adityadhammawantang@gmail.com>
2017-04-19 16:09:00 +0700

    Update numbers.js

--- values-types-and-operators/numbers.js
+++ values-types-and-operators/numbers.js
@@ -7,7 +7,7 @@ var annualLeave = 30
 var BobRemainingDays = 12
 var AliceRemainingDays = 28
 var TomRemainingDays = 19
-var average = annualLeave-((BobRemainingDays+AliceRemainingDays+TomRemainingDays) / 3.0)
+var average = 30-(59.0/3.0)
 
 exports.average = average
 exports.annualLeave = annualLeave
