da1551810c517c6b23cc687cdfaa9facdf33cbe1
adityadhammawantang <adityadhammawantang@gmail.com>
2017-04-19 16:18:03 +0700

    Update numbers.js

--- values-types-and-operators/numbers.js
+++ values-types-and-operators/numbers.js
@@ -3,14 +3,14 @@
  * Write an expression that calculates the average number of leave days taken by the employees in this year. Replace your_answer with your expression.
  */
 
-var annualLeave = 30
-var BobRemainingDays = 12
-var AliceRemainingDays = 28
-var TomRemainingDays = 19
-var average = 30.0-(59.0/3.0)
+var annualLeave = 30;
+var BobRemainingDays = 12;
+var AliceRemainingDays = 28;
+var TomRemainingDays = 19;
+var average = 30.0-(59.0/3.0);
 
-exports.average = average
-exports.annualLeave = annualLeave
-exports.BobRemainingDays = BobRemainingDays
-exports.AliceRemainingDays = AliceRemainingDays
-exports.TomRemainingDays = TomRemainingDays
+exports.average = average;
+exports.annualLeave = annualLeave;
+exports.BobRemainingDays = BobRemainingDays;
+exports.AliceRemainingDays = AliceRemainingDays;
+exports.TomRemainingDays = TomRemainingDays;
